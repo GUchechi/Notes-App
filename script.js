4e720711f4192694d3c1f84cda2bfc589e19a23c
GUchechi <godswilluchendu7@gmail.com>
2022-10-03 22:06:56 +0100

    notes app

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 const addBtn = document.getElementById('add')
 
+const notes = JSON.parse(localStorage.getItem('notes'))
+
+if(notes) {
+    notes.forEach(note => addNewNote(note))
+}
+
 addBtn.addEventListener('click', () => addNewNote())
 
 function addNewNote(text = '') {
-    const note = document.createElement('div');
-    note.classList.add('note');
+    const note = document.createElement('div')
+    note.classList.add('note')
 
     note.innerHTML = `
     <div class="tools">
         <button class="edit"><i class="fas fa-edit"></i></button>
-        <button class="delete"><i class="fas fa-trash"></i></button>
+        <button class="delete"><i class="fas fa-trash-alt"></i></button>
     </div>
-
     <div class="main ${text ? "" : "hidden"}"></div>
     <textarea class="${text ? "hidden" : ""}"></textarea>
     `
@@ -25,7 +30,9 @@ function addNewNote(text = '') {
     main.innerHTML = marked(text)
 
     deleteBtn.addEventListener('click', () => {
-        note.remove();
+        note.remove()
+
+        updateLS()
     })
 
     editBtn.addEventListener('click', () => {
@@ -38,10 +45,18 @@ function addNewNote(text = '') {
 
         main.innerHTML = marked(value)
 
-        // updateLS()
+        updateLS()
     })
 
     document.body.appendChild(note)
 }
 
+function updateLS() {
+    const notesText = document.querySelectorAll('textarea')
 
+    const notes = []
+
+    notesText.forEach(note => notes.push(note.value))
+
+    localStorage.setItem('notes', JSON.stringify(notes))
+}
